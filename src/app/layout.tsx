da537ddc40804167ae54f5b1e62af9947bbcd87c
Vinícius Zanotti de Oliveira <vinizanotti@gmail.com>
2025-10-06 22:34:22 -0300

    Head and Body tags for Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -97,8 +97,30 @@ export default function RootLayout({
           src={`https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js`}
           crossOrigin="anonymous"
         />
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N2GC96MN');
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N2GC96MN"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
         <Header />
         {children}
         <Footer />
